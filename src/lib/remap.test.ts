f135883edabeda85dbc4a0387f8b5879101789ed
hackape <muse.vincent@gmail.com>
2018-04-11 21:51:54 +0800

    unify code style of test

--- src/lib/remap.test.ts
+++ src/lib/remap.test.ts
@@ -3,50 +3,46 @@ import remap from './remap'
 test('basic test', () => {
   const source = { hello: 'world' }
   const target = { hello: 'world' }
-  expect(
-    remap(source, types => {
-      const { string } = types
-      return { hello: string }
-    })
-  ).toEqual(target)
+  const result = remap(source, types => {
+    const { string } = types
+    return { hello: string }
+  })
+  expect(result).toEqual(target)
 })
 
 test('use `from()` to rename key', () => {
   const source = { hello: 'world' }
   const target = { hola: 'world' }
-  expect(
-    remap(source, types => {
-      const { from } = types
-      return { hola: from('hello').string }
-    })
-  ).toEqual(target)
+  const result = remap(source, types => {
+    const { from } = types
+    return { hola: from('hello').string }
+  })
+  expect(result).toEqual(target)
 })
 
 test('use accept plain object as spec value', () => {
   const source = { data: { clicks: 100, costs: 150 } }
   const target = { data: { clicks: 100, costs: 150 } }
-  expect(
-    remap(source, types => {
-      const { number } = types
-      return { data: { clicks: number, costs: number } }
-    })
-  ).toEqual(target)
+  const result = remap(source, types => {
+    const { number } = types
+    return { data: { clicks: number, costs: number } }
+  })
+  expect(result).toEqual(target)
 })
 
 test('`from()` in nested level', () => {
   const source = { data: { clicks: 100, costs: 150 } }
   const target = { data: { visits: 100, costs: 150 } }
-  expect(
-    remap(source, types => {
-      const { number, from } = types
-      return {
-        data: {
-          visits: from('clicks').number,
-          costs: number
-        }
+  const result = remap(source, types => {
+    const { number, from } = types
+    return {
+      data: {
+        visits: from('clicks').number,
+        costs: number
       }
-    })
-  ).toEqual(target)
+    }
+  })
+  expect(result).toEqual(target)
 })
 
 test('use `compute()` to return computed value', () => {
@@ -56,7 +52,7 @@ test('use `compute()` to return computed value', () => {
     const { from, compute } = types
     return {
       percentage: from('ratio').compute((ratio: number) => `${ratio * 100}%`),
-      ratio: compute(ratio => ratio + 1)
+      ratio: compute((ratio: number) => ratio + 1)
     }
   })
   expect(result).toEqual(target)
